3b119b84ec2cbaf113a27d8b7572386b96adc5c4
shaily29-eng <shgo4037@colorado.edu>
2024-06-01 22:17:48 -0600

    initial

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
+console.log('MONGO_URL:', process.env.MONGO_URL); 
+
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
